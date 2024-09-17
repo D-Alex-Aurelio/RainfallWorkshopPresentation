b5926056783115146a8a5a0914d9c234d5dc5606
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2024-09-17 22:04:03 +0800

    added more info

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from streamlit import expander
+
+st.title("Data Driven Rainfall Management:")
+st.header("Building an Interactive Dashboard for "
+          "Rainfall Trends and Statistics")
+
+
+st.divider()
+st.header("Presentation Flow:")
+st.markdown("### Data Driven Management")
+st.write("What ")
+st.write(" ")
+st.markdown("### Python in Data Analysis")
+st.write("Where to code the dashboard?")
+st.write(" ")
+st.markdown("### Rainfall Dashboard Demo")
+st.page_link("https://rainfalldashboardapp-hks9gxfbbsrskaslq846ip.streamlit.app/",
+             label="Rainfall Analysis Dashboard")
