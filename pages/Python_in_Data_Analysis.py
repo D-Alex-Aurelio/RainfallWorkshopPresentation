bc55df807aee31b3b54868a3141227dfdb7bc3ae
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2024-09-17 22:29:50 +0800

    Added information to Python section

--- pages/Python_in_Data_Analysis.py
+++ pages/Python_in_Data_Analysis.py
@@ -3,5 +3,27 @@ import streamlit as st
 
 st.title("Python in Data Analysis")
 st.header("Significance of Python in Data Analysis")
+st.write("- Simple syntax")
+st.write("- Wide range of libraries")
+
+st.divider()
+
 st.header("Integrated Development Environment (IDEs)")
-st.header("Popular Python Libraries for Dashboard Creation")
+st.write("### PyCharm")
+st.write("- From JetBrains, used to create Python applications")
+st.write("### Google Colab / JupyterLab")
+st.write("- Uses cells to separate code into parts")
+st.write("- Optimal for step-by-step applications")
+
+st.divider()
+
+st.header("Python Libraries for Dashboard Creation")
+st.write("### Pandas")
+st.write("- Used for reading data files")
+st.write("### Seaborn")
+st.write("- Used for plotting data")
+st.write("- Creates static plots")
+st.write("### Plotly")
+st.write("- Used to create interactive plots")
+st.write("### Streamlit")
+st.write("- Used to create web applications")
